5ca3c1e3b4a0ed94e48e96d3e8e8a5e23ec843a0
mrsamir <sirsamir@outlook.com>
2022-06-01 21:50:27 +0600

    added react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './../node_modules/bootstrap/dist/js/bootstrap.min.js';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <h2>main app</h2>
     </div>
   );
 }
